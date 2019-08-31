719ce008ee5f8183a6f7b68ce02d449deb56b85c
Noman Mustafa <47340055+moon341@users.noreply.github.com>
2019-08-31 20:53:19 +0800

    Update graph_dict.py

--- graph_dict.py
+++ graph_dict.py
@@ -1,22 +1,17 @@
 def graph_dict(tuple_of_connected_nodes):
-    c={}
-    f=[]
-    g=0
+    dicti={}
     for key,value in tuple_of_connected_nodes:
-        if key != g:
-            g=key
-            #c={key:f}
-            del f
-            f=[]
-            f.append(value)
-            #c={key:f}
-            c.update({key:f})
-        elif key == key:
-            g=key 
-            f.append(value)
-            continue
-    return c
+        if key in dicti:
+            temp=[]
+            temp = dicti[key]
+            temp.append(value)
+            dicti.update({key:temp})
+        else:
+            temp=[]
+            temp.append(value)
+            dicti.update({key:temp})
+    return dicti
 
-d=((1,2),(2,1),(2,6),(2,7),(2,3),(3,4),(3,2),(3,5),(4,2),(4,3),(4,5),(5,3),(5,4),(6,2),(7,2),(8,9),(9,8))
+d=((1,2),(2,6),(2,7),(2,3),(3,4),(2,1),(3,2),(3,5),(4,2),(4,5),(5,3),(5,4),(6,2),(4,3),(7,2),(8,9),(9,8))
 t=graph_dict(d)
 print(t)
